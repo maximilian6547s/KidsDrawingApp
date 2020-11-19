42c57bfa0b002aa8cf9b979420bd8342be9be82b
Maximilian6547s <maximilian.s517@gmail.com>
2020-11-19 17:50:48 +0300

    Add dialog to change brush size

--- app/src/main/java/com/maximcuker/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/maximcuker/kidsdrawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.maximcuker.kidsdrawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -30,7 +31,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mDrawPaint?.strokeJoin = Paint.Join.ROUND
         mDrawPaint?.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+        //mBrushSize = 20.toFloat()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -84,8 +85,13 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
 
     }
 
-    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
+    fun setSizeForBrush(newSize: Float) {
+        //convert float to DP dependency pixel size
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, resources.displayMetrics)
+        mDrawPaint?.strokeWidth = mBrushSize
+    }
 
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
     }
 
 }
--- app/src/main/java/com/maximcuker/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/maximcuker/kidsdrawingapp/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.maximcuker.kidsdrawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawingView.setSizeForBrush(20.toFloat())
+
+        ibBrush.setOnClickListener {
+            showBrushChooseSizeDialog()
+        }
+    }
+
+    private fun showBrushChooseSizeDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+        val smallButton = brushDialog.ibSmallBrush
+        smallButton.setOnClickListener {
+            drawingView.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+        val mediumButton = brushDialog.ibMediumBrush
+        mediumButton.setOnClickListener {
+            drawingView.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+        val largeBrush = brushDialog.ibLargeBrush
+        largeBrush.setOnClickListener {
+            drawingView.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
